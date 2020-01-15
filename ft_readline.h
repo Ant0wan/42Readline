082aadb7a858254e74a945308d831943988bb38b
Antoine Barthelemy <abarthel@student.42.fr>
2020-01-15 15:54:32 +0100

    feat(tc): init termcaps

--- ft_readline.h
+++ ft_readline.h
@@ -1,13 +1,15 @@
 #ifndef FT_READLINE_H
 # define FT_READLINE_H
 
-#include <curses.h>
-#include <term.h>
-#include <termios.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/ioctl.h>
-#include <unistd.h>
+# include <curses.h>
+# include <term.h>
+# include <termios.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/ioctl.h>
+# include <unistd.h>
+
+# define NUM_TC_STRINGS (sizeof(tc_strings) / sizeof(struct _tc_string))
 
 struct s_termcaps
 {
@@ -24,6 +26,12 @@ struct s_termcaps
 	char	*kr;
 };
 
+struct termcaps_string
+{
+	const char *const	var;
+	char			**value;
+};
+
 /* Variables that hold the screen dimensions, used by the display code. */
 struct s_screen
 {
@@ -32,10 +40,11 @@ struct s_screen
 	int	chars;
 };
 
-extern struct s_termcaps	g_tc;
-extern struct termios		g_termios;
-extern struct termios		g_termios_backup;
-extern struct s_screen		g_screen;
+extern struct s_termcaps		g_tc;
+extern struct termios			g_termios;
+extern struct termios			g_termios_backup;
+extern struct s_screen			g_screen;
+extern const struct termcaps_string	*g_tc_strings;
 
 int	get_screensize(int tty);
 int	is_interactive(void);
--- terminal.c
+++ terminal.c
@@ -1,6 +1,57 @@
 #include "ft_readline.h"
 
-struct s_screen	g_screen;
+struct s_screen			g_screen;
+
+const struct termcaps_string	g_tc_strings[] =
+{
+	{"@7", &term_at7},
+	{"DC", &term_DC},
+	{"E3", &term_clrscroll},
+	{"IC", &term_IC},
+	{"ce", &term_clreol},
+	{"cl", &term_clrpag},
+	{"cr", &term_cr},
+	{"dc", &term_dc},
+	{"ei", &term_ei},
+	{"ic", &term_ic},
+	{"im", &term_im},
+	{"kD", &term_kD},	/* delete */
+	{"kH", &term_kH},	/* home down ?? */
+	{"kI", &term_kI},	/* insert */
+	{"kd", &term_kd},
+	{"ke", &term_ke},	/* end keypad mode */
+	{"kh", &term_kh},	/* home */
+	{"kl", &term_kl},
+	{"kr", &term_kr},
+	{"ks", &term_ks},	/* start keypad mode */
+	{"ku", &term_ku},
+	{"le", &term_backspace},
+	{"mm", &term_mm},
+	{"mo", &term_mo},
+	{"nd", &term_forward_char},
+	{"pc", &term_pc},
+	{"up", &term_up},
+	{"vb", &visible_bell},
+	{"vs", &term_vs},
+	{"ve", &term_ve},
+};
+
+
+/* Inititalize termcaps. Get termcaps values. */
+int	get_term_capabilities(char **bp)
+{
+	register int	i;
+
+	i = 0;
+	while (i < NUM_TC_STRINGS)
+	{
+		*(g_tc_strings[i].value) = tgetstr((char *)tc_strings[i].var, bp);
+		if (g_tc_string[i] == NULL)
+			return (-1);
+		++i;
+	}
+	return (0);
+}
 
 int		get_screensize(int tty)
 {
@@ -27,6 +78,9 @@ static int	redisplay_after_sigwinch(void)
            either more or fewer characters on that line only) and put the cursor at
            column 0.  Make sure the right thing happens if we have wrapped to a new
            screen line. */
+
+	/* TO DO */
+
 }
 
 /* Redisplay the current line after a SIGWINCH is received. */
