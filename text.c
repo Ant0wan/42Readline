e8dd189c9f048c8a74affdf3fef7994fc5a28009
Antoine Barthelemy <abarthel@student.42.fr>
2020-01-17 17:38:04 +0100

    feat(keymap): adding binding in keymap ctrlx

--- text.c
+++ text.c
@@ -4,3 +4,23 @@ void	rl_insert(int c)
 {
 	output(c);
 }
+
+void	cursor_l(void)
+{
+	return;
+}
+
+void	cursor_r(void)
+{
+	return;
+}
+
+void	history_up(void)
+{
+	return;
+}
+
+void	history_down(void)
+{
+	return;
+}
