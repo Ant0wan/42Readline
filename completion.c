567406baacb1116409090bb0f9ae6010515f9391
Antoine Barthelemy <abarthel@student.42.fr>
2020-02-02 12:01:27 +0100

    feat(completion): adding the completion, bin suggest

--- completion.c
+++ completion.c
@@ -5,7 +5,7 @@ char	*suggest_bin(void)
 	return (NULL);
 }
 
-char	*sugget_file(void)
+char	*sugget_file()
 {
 	return (NULL);
 }
@@ -16,8 +16,31 @@ char	*suggest(void)
 	char	*word;
 
 	i = g_line_state_invisible.len - 1;
-	if (i >= 0 && g_line_state_invisible.line[i] == 'l')
-		return (rl_strdup("s")); /* send the diff only */
-	else
-		return (NULL);
+	while (i >= 0)
+	{
+		if (g_line_state_invisible.line[i] == ' ')
+			--i;
+		else if (isalnum(g_line_state_invisible.line[i]) == ';'
+				|| isalnum(g_line_state_invisible.line[i]) == '&'
+				|| isalnum(g_line_state_invisible.line[i]) == '|')
+		{
+			suggest_bin();
+		}
+		else if (isalnum(g_line_state_invisible.line[i]))
+		{
+			while (i > 0
+				&& isalnum(g_line_state_invisible.line[i])
+				&& isalnum(g_line_state_invisible.line[i]) != ';'
+				&& isalnum(g_line_state_invisible.line[i]) != '&'
+				&& isalnum(g_line_state_invisible.line[i]) != '|')
+				--i;
+			if (i != 0 || !isalnum(g_line_state_invisible.line[i])
+					|| isalnum(g_line_state_invisible.line[i]) == ';'
+					|| isalnum(g_line_state_invisible.line[i]) == '&'
+					|| isalnum(g_line_state_invisible.line[i]) == '|')
+				++i;
+			return (suggest_file(&(g_line_state_invisible.line[i])));
+		}
+	}
+	return (NULL);
 }
