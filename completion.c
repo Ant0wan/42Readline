96edd1723d3325f8d29cf347594974ae40fc64fc
Antoine Barthelemy <abarthel@student.42.fr>
2020-02-02 16:19:44 +0100

    fix(completion): on the go

--- completion.c
+++ completion.c
@@ -16,40 +16,53 @@ static char	*suggest_file(const char *beg_s)
 	return (file);
 }
 
+static int	count_word(int i)
+{
+	int	c;
+	int	b;
+
+	c = 0;
+	while (g_line_state_invisible.line[i] && i < g_display.cpos_buffer_position)
+	{
+		b = 0;
+		while (g_line_state_invisible.line[i] == ' ')
+			++i;
+		while (g_line_state_invisible.line[i] && g_line_state_invisible.line[i] != ' ')
+		{
+			if (g_line_state_invisible.line[i] == '|'
+				|| g_line_state_invisible.line[i] == ';'
+				|| g_line_state_invisible.line[i] == '&')
+				c = 0;
+			b |= 1;
+			++i;
+		}
+		if (b)
+			++c;
+	}
+//	printf("\n\n|%c|%d\n\n", g_line_state_invisible.line[g_display.cpos_buffer_position - 1], c);
+	if (g_line_state_invisible.line[g_display.cpos_buffer_position - 1] != ' ')
+		return (c - 1);
+	else
+		return (c);
+}
+
 char	*suggest(void)
 {
 	int	i;
-	int	flag;
-	char	*word;
 
-	flag = 0;
-	i = g_line_state_invisible.len - 1;
-	while (i >= 0 && g_line_state_invisible.line[i])
+	i = g_display.cpos_buffer_position;
+	if (i == 0)
+		return (suggest_bin(""));
+	while (i > 0)
 	{
-//		printf("\n>   %s\n", &(g_line_state_invisible.line[i]));
-//		printf("%d\n", flag);
-		if (flag == 1)
-			return (suggest_file(&(g_line_state_invisible.line[i])));
 		if (g_line_state_invisible.line[i] == ';'
 			|| g_line_state_invisible.line[i] == '&'
 			|| g_line_state_invisible.line[i] == '|')
-			return (suggest_bin(&(g_line_state_invisible.line[i])));
-//		printf("\n>>  %s\n", &(g_line_state_invisible.line[i]));
-		while (i >= 0 && g_line_state_invisible.line[i] == ' ')
-			--i;
-//		if (i >= 0)
-//			printf("\n>>> %s\n", &(g_line_state_invisible.line[i]));
-		while (i > 0 && (g_line_state_invisible.line[i] != ' '
-				|| g_line_state_invisible.line[i] != ';'
-				|| g_line_state_invisible.line[i] != '&'
-				|| g_line_state_invisible.line[i] != '|'))
-		{
-//			printf("\n>>>>%s\n", &(g_line_state_invisible.line[i]));
-			flag = 1;
-			--i;
-		}
-//		if (i >= 0)
-//			printf("\n>>>>%s\n", &(g_line_state_invisible.line[i]));
+			break;
+		--i;
 	}
+//	write(STDOUT_FILENO, &(g_line_state_invisible.line[i]), &(g_line_state_invisible.line[g_display.cpos_buffer_position]) - &(g_line_state_invisible.line[i]));
+//	printf("\n\n%s\n\n", &(g_line_state_invisible.line[i]));
+	printf("\n%d\n", count_word(i));
 	return (suggest_bin(""));
 }
