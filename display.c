0e5e901ac9b41dd6cb1afd637241517de4a0c5c4
Antoine Barthelemy <abarthel@student.42.fr>
2020-03-05 12:45:16 +0100

    fix(versionning): attempt to fix the mess in versions...

--- display.c
+++ display.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 17:20:42 by abarthel          #+#    #+#             */
-/*   Updated: 2020/03/04 19:40:08 by snunes           ###   ########.fr       */
+/*   Updated: 2020/03/05 12:42:40 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,8 @@ struct s_line_state	g_line_state_invisible = {0};
 
 void		display_prompt(void)
 {
-//	ft_printf("|b%.3dc%.3dv%.3d|> ", g_display.vis_botlin, g_cursor.last_c_pos ,g_cursor.last_v_pos);
-	write(STDOUT_FILENO, g_display.display_prompt, g_display.visible_prompt_length);
+	ft_printf("|b%.3dc%.3dv%.3d|> ", g_display.vis_botlin, g_cursor.last_c_pos ,g_cursor.last_v_pos);
+//	write(STDOUT_FILENO, g_display.display_prompt, g_display.visible_prompt_length);
 }
 
 void		set_prompt(const char *prompt)
@@ -51,7 +51,7 @@ void		set_prompt(const char *prompt)
 	else
 		g_display.display_prompt = g_display.prompt;
 	g_display.visible_prompt_length = 16;
-	g_display.visible_prompt_length = ft_strlen(g_display.display_prompt); /* Assume single line prompt and does not handle '\' all chr */
+//	g_display.visible_prompt_length = ft_strlen(g_display.display_prompt); /* Assume single line prompt and does not handle '\' all chr */
 	g_display.visible_first_line_len = g_screen.width - g_display.visible_prompt_length;
 }
 
@@ -66,10 +66,12 @@ void	display_lines(void)
 	g_display.visible_first_line_len = g_screen.width - g_display.visible_prompt_length;
 	/*why writing g_display.visible_first_line_len and not g_line_state_invisible.len ?*/
 	write(STDOUT_FILENO, g_line_state_invisible.line, g_display.visible_first_line_len);
-	/* Leave newline to autowrap... I know... */
-//	if (g_line_state_invisible.len >= g_display.visible_first_line_len)
-//		ft_putstr(tgoto(g_termcaps.do1, 0, 0));
-//		write(STDOUT_FILENO, "\n", 1);
+	/* Leave newline to autowrap... */
+	if (!g_term.autowrap)
+	{
+		if (g_line_state_invisible.len >= g_display.visible_first_line_len)
+			ft_putstr(tgoto(g_termcaps.do1, 0, 0));
+	}
 	chr_l -= g_display.visible_first_line_len;
 	index += g_display.visible_first_line_len;
 	while (chr_l > 0)
@@ -79,7 +81,8 @@ void	display_lines(void)
 			write(STDOUT_FILENO, &(g_line_state_invisible.line[index]), g_screen.width);
 			chr_l -= g_screen.width;
 			index += g_screen.width;
-//			write(STDOUT_FILENO, "\n", 1);
+	 		if (!g_term.autowrap) /* autowrap */
+				ft_putstr(tgoto(g_termcaps.do1, 0, 0));
 		}
 		else if (chr_l > 0)
 		{
@@ -89,7 +92,6 @@ void	display_lines(void)
 	}
 	if ((g_display.visible_prompt_length + g_line_state_invisible.len) % g_screen.width == 0)
 	 	ft_putstr(tgoto(g_termcaps.do1, 0, 0));
-	//	write(STDOUT_FILENO, "\n", 1);
 }
 
 void	update_line(void)
